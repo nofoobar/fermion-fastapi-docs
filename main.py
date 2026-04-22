e3b0c89f2c32d235ee03abdcc4168bfad9b50cbf
Sourabh Sinha <sourabhsinha396@gmail.com>
2026-04-22 15:38:42 +0530

    add readme and sample project

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="FastAPITutorial.com", docs_url="/")
+
+
+@app.get("/message")
+def read_root():
+    # TODO: Return a dictionary with the key "hello" and the value "world"
+    return {"message": "return hello world here"}
